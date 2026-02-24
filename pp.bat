12cfb2abf134aa9acd1696a5df2f902eba0f1a53
nabe-abk <339449+nabe-abk@users.noreply.github.com>
2026-02-24 23:44:57 +0900

    Update: adiary.exe, use strawberry-perl-5.42.0.1

--- pp.bat
+++ pp.bat
@@ -1,6 +1,5 @@
-for /f "usebackq tokens=*" %%i in (`perl -e "use Image::Magick;my $p=$INC{'Image/Magick.pm'}; $p =~ s|/[^/]*$||; print $p"`) do @set MAGICK_PATH=%%i
 @rem
-pp @__tools/pp.opt -a %MAGICK_PATH%;site/lib/Image
+pp @__tools/pp.opt
 @rem
 @rem A checksum error occurs in the file with icon replaced
 @rem perl -e "use Win32::Exe; $exe = Win32::Exe->new('adiary.exe'); $exe->set_single_group_icon('__tools/pp.ico'); $exe->write;"
